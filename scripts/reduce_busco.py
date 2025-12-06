1c9581620d3e3e26edb71725e0f0f2586048a064
Matthieu Muffato <mm49@sanger.ac.uk>
2025-12-06 13:26:36 +0000

    Fixed the import order

--- scripts/reduce_busco.py
+++ scripts/reduce_busco.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-from collections.abc import Container, Sized
 import os
-from pathlib import Path
 import shutil
+from collections.abc import Container, Sized
+from pathlib import Path
 from typing import Dict, List
 
 
