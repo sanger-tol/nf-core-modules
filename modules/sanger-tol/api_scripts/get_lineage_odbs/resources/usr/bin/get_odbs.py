64294dadcd52ce9a05057c2705da8097ea51ae32
DLBPointon <damonlbp@hotmail.co.uk>
2026-06-04 11:50:00 +0100

    Update script for real fix

--- modules/sanger-tol/api_scripts/get_lineage_odbs/resources/usr/bin/get_odbs.py
+++ modules/sanger-tol/api_scripts/get_lineage_odbs/resources/usr/bin/get_odbs.py
@@ -165,7 +165,7 @@ def get_specific_odbs(
     User provided lineages will have the ODB
     """
     specified_odbs = [validate_lineage(i + odb_version, lineage_path) for i in specified_lineages]
-    return specified_odbs + [i + odb_version for i in basal_lineages]
+    return specified_odbs + basal_lineages
 
 
 def get_mapping_file(odb_version: str):
@@ -227,7 +227,7 @@ def main(args=None):
                 args.odb_dir,
                 args.basal_lineages,
                 odb_version_string,
-                args.basal_lineages,
+                new_basal_lineages,
             )
 
         all_lineages.extend(lineage_list)
